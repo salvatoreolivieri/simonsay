a769964558d4f6b85f8eaec98411d5ce64f07df8
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-26 17:36:14 +0200

    Esercizio completato

--- script.js
+++ script.js
@@ -4,22 +4,25 @@
 2. Dopo 5 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 3. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
-
+const outputMessage = document.getElementById("output-message")
 const boxContainer = document.querySelector(".box-container");
 let listNumbers = [];
+let listUserNumbers = [];
 
+
+// Comando di inizio
 init();
 
 function init() {
 
   // Ciclo for che si collega alla funzione dei numeri randomici
-  for (let x = 0; x < 5; x++) {
+  for (let x = 1; x < 6; x++) {
 
     let randomNumber = getUniqueRandomNumber(99, 1);
     console.log(randomNumber);
 
     const square = document.createElement("div");
-    square.classList.add("square");
+    square.classList.add("square-"+x);
     square.innerHTML = randomNumber;
 
     boxContainer.append(square);
@@ -33,11 +36,44 @@ function init() {
   // Timer di 5 secondi
   setTimeout(() => {
     for (let x = 0; x < 5; x++) {
-      // prompt("Inserisci uno alla volta, i numeri che hai visto")
+
+      askNumber();
+
+      // Facciamo un check tra gli array e vediamo quanti ne ha indovinato
+      checkNumber();
+
     }
   }, 5000);
 
 }
+
+
+//Funzione che chiedi i numeri all'utente
+
+function askNumber() {
+
+  const userNumber = parseInt(prompt("Inserisci uno alla volta, i numeri che hai visto"));
+
+  console.log(userNumber);
+  listUserNumbers.push(userNumber); //Inseriamo i numeri inseriti dall'utente nell'array "listUserNumbers"
+  console.log(listUserNumbers);
+
+}
+
+
+//Funzione che controlla i numeri chiesti all'utente con quelli presenti nell'array dei random number
+
+function checkNumber() {
+  const intersection = listNumbers.filter(element => listUserNumbers.includes(element));
+
+  const numeriIndovinari =  intersection;
+
+  outputMessage.innerHTML = 
+  `Hai indovinato ${intersection.length} numeri. I seguenti: ${numeriIndovinari}.
+  `
+  console.log(intersection);
+}
+
 
 
 
