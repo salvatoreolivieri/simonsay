347596235bb7307113764605ed400b7659ca4a85
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-26 15:38:24 +0200

    Aggiunto stile e creata funzione per estrazione numero random unico

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 /* Descrizione:
 1. Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 5 secondi.
 2. Dopo 5 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+3. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
+
+const boxContainer = document.querySelector(".box-container");
+let listNumbers = [];
 
 init();
 
@@ -11,14 +15,26 @@ function init() {
   // Ciclo for che si collega alla funzione dei numeri randomici
   for (let x = 0; x < 5; x++) {
 
-    let randomNumber = getRandomNumber(99, 1);
+    let randomNumber = getUniqueRandomNumber(99, 1);
     console.log(randomNumber);
+
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.innerHTML = randomNumber;
+
+    boxContainer.append(square);
+
+
+
+    square.innerhtml += randomNumber;
 
   }
 
   // Timer di 5 secondi
   setTimeout(() => {
-    console.log("sono passati 5 secondi");
+    for (let x = 0; x < 5; x++) {
+      // prompt("Inserisci uno alla volta, i numeri che hai visto")
+    }
   }, 5000);
 
 }
@@ -27,7 +43,26 @@ function init() {
 
 //Funzione che genera 5 numeri casuali
 
-function getRandomNumber(max, min) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+function getUniqueRandomNumber(min, max){
+  let number = null;
+  let valid = false;
 
+  // se il numero è presente nell'array globale listNumbers valid è false quindi ne estraggo un'altro e così via
+  while(!valid){
+      number = getRandomNumber(min, max);
+      // se listNumbers non contiene number allora il numero è valido
+      if(!listNumbers.includes(number)){
+          // valid diventa true e blocca il ciclo
+          valid = true;
+          // aggiungo il numero alla lista
+          listNumbers.push(number)
+      }
+  }
+
+  return number;
 }
+
+  function getRandomNumber(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+
+  }
